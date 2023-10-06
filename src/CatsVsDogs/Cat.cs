1ecca08dd5221023df4f700f463759d282c6134e
Sersho <Email>
2023-10-06 22:06:35 +0300

    Update

---
+++ src/CatsVsDogs/Cat.cs
@@ -0,0 +1,28 @@
+﻿namespace Cats;
+
+public class Cat
+{
+    public string Name { get; set; } // Свойство: Имя
+    public int Age { get; set; }    // Свойство: Возраст
+    public int Power { get; set; }  // Свойство: Сила
+
+    // При создании класса конструктор выполняется первым.
+    public Cat(string name, int age, int power)
+    {
+        Name = name;
+        Age = age;
+        Power = power;
+    }
+
+    // Метод: Укус
+    public void Bite()
+    {
+        Console.WriteLine("Я укусил");
+    }
+
+    // Метод: Поздороваться
+    public void SayHello(string greetingText)
+    {
+        Console.WriteLine($"{greetingText}! Меня зовут {Name}");
+    }
+}
---
+++ src/CatsVsDogs/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Cats;
+
+// 1. Нам нужны котики
+// 1.1. У них должны быть:
+//      - Имя, Сила, Возраст
+// 1.2. Они должны уметь:
+//     - Кусаться, Здороваться.
+
+public class Program
+{
+    public static void Main()
+    {
+        var cat = new Cat( "Barnie",3,20);
+
+        Console.WriteLine(cat.Name);
+        Console.WriteLine(cat.Age);
+        Console.WriteLine(cat.Power);
+
+        // Укусить.
+        cat.Bite();
+
+        // Поздороваться
+        cat.SayHello("Здарова, заебал");
+
+        Console.ReadLine();
+    }
+}
+
